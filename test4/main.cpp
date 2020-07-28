b8b49616805144d4dde48bf20fb57ac2a7aab4ba
TomGrimwood <tgri417@aucklanduni.ac.nz>
2020-07-28 22:04:08 +1200

    Initial Commit

---
+++ test4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test4
+//
+//  Created by Tom Grimwood on 28/07/20.
+//  Copyright © 2020 Tom Grimwood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
