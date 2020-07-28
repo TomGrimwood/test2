99ef5f24907e02c8d320449a35e9d86c1f37cc02
TomGrimwood <tgri417@aucklanduni.ac.nz>
2020-07-28 22:10:29 +1200

    Initial Commit

---
+++ wefwef/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wefwef
+//
+//  Created by Tom Grimwood on 28/07/20.
+//  Copyright © 2020 Tom Grimwood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
